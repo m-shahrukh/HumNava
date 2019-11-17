5b923e0e56c741457de838405cbc3295bb0273ec
Muhammad Shahrukh <20100287@lums.edu.pk>
2019-11-17 17:52:48 +0500

    Made screen 1

---
+++ lib/landingpage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:hum_nava/register.dart';
+
+
+class HumNavaLandingPage extends StatelessWidget
+{
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+            backgroundColor: Colors.white,
+            body: 
+            Center(
+            child:Column(
+              
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget> [ 
+
+              Container(child:
+              Image.asset('assets/Logo.jpeg'),
+              margin: EdgeInsets.only(top:175.0),
+              width: 300,
+              height: 120,
+              ),
+              
+              Container(
+              margin: EdgeInsets.all(30.0),
+              width: double.infinity,  //makes container take entire width of screen
+              child: Text(
+              "HamNava",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center    
+
+              ),
+            ),
+
+            Column(
+              //mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top:10.0),
+                width: 195.00,
+                height: 50.00,
+                //margin: EdgeInsets.only(bottom: 60.0),
+                child: RaisedButton(color: Colors.green,
+                textColor: Colors.white,
+                child: Text(
+                  "Register",
+                   style: TextStyle(fontSize: 23),
+                   textAlign: TextAlign.center,
+
+                
+                ),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> Register()));
+                }
+                ),
+                
+              ),
+              Container(
+                margin: EdgeInsets.only(top:40.0),
+                width: 195.00,
+                height: 50.00,
+                           //     margin: EdgeInsets.only(bottom: 60.0),
+                child: RaisedButton(color: Colors.green,
+                textColor: Colors.white,
+                child: Text("Translate",
+                                   style: TextStyle(fontSize: 23),
+                   textAlign: TextAlign.center,),
+                onPressed: (){}
+                ),
+              ),
+              ]),
+              ]
+            )
+            )
+          );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class Register extends StatelessWidget
+{
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(appBar:
+     AppBar(backgroundColor: Colors.white,
+     iconTheme: IconThemeData(color: Colors.black),
+    title: Text('Personal Information',
+    style: TextStyle(fontSize: 22,
+    color: Colors.black,
+    ),
+    )
+    ),
+    backgroundColor: Colors.white,
+    body:
+    Center(child:Container(
+      margin: EdgeInsets.only(top:20.0),
+      alignment: Alignment.topCenter,
+      width: 250,
+      child:TextFormField(
+  decoration: InputDecoration(
+    labelText: 'Name',
+    
+  ),
+), 
+),
+)
+    );
+  }
+}
