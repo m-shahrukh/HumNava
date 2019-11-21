b681352bb3c1a38845c50274c77eb671d6e3579c
Muhammad Shahrukh <20100287@lums.edu.pk>
2019-11-21 14:51:56 +0500

    added image. added asset to pubspec.yaml

--- lib/register.dart
+++ lib/register.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
 
-
-class Register extends StatelessWidget
-{
-  @override 
+class Register extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar:
-     AppBar(backgroundColor: Colors.white,
-     iconTheme: IconThemeData(color: Colors.black),
-    title: Text('Personal Information',
-    style: TextStyle(fontSize: 22,
-    color: Colors.black,
-    ),
-    )
-    ),
-    backgroundColor: Colors.white,
-    body:
-    Center(child: Column(
-      children: <Widget>[    
-    Container(
-      margin: EdgeInsets.only(top:20.0),
-      alignment: Alignment.topCenter,
-      width: 250,
-      child:TextFormField(
-  decoration: InputDecoration(
-    labelText: 'Name',
-    
-  ),
-), 
-),
-   Container(
-      margin: EdgeInsets.only(top:20.0),
-      alignment: Alignment.topCenter,
-      width: 250,
-      child:TextFormField(
-  decoration: InputDecoration(
-    labelText: 'Phone Number',
-    
-  ),
-), 
-)
-]
-    ),
-),
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            'Personal Information',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.black,
+            ),
+          )),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            alignment: Alignment.topCenter,
+            width: 250,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Name',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            alignment: Alignment.topCenter,
+            width: 250,
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+              ),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.all(40.0),
+              width: 175.0,
+              height: 190.0,
+              decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: ExactAssetImage('assets/DP-1.jpeg')))),
+          Text("Shahrukh", textScaleFactor: 1.5)
+        ]),
+      ),
     );
   }
 }
